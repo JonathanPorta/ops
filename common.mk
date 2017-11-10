0cdfa22660815ecc7049dd48a010ad467a6a909d
Jonathan Porta <jonathan@jonathanporta.com>
2017-11-10 06:39:03 -0700

    Fix testing and add a common makefile for shell scripting

--- common.mk
+++ common.mk
@@ -3,10 +3,18 @@ TEMP_DIR=$(shell pwd)/tmp
 
 include ops/aws-lambda.mk
 include ops/ci.mk
+include ops/shell.mk
+
+
+# Figure out which OS we are running.
+OS="linux"
+ifeq ("$(shell uname)", "Darwin"); then
+	OS="darwin"
+endif
 
 common_terraform_binary:
-	curl https://releases.hashicorp.com/terraform/0.10.7/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -o /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-	unzip -o /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d ${TEMP_DIR}/
+	curl https://releases.hashicorp.com/terraform/0.10.7/terraform_${TERRAFORM_VERSION}_${OS}_amd64.zip -o /tmp/terraform_${TERRAFORM_VERSION}_${OS}_amd64.zip
+	unzip -o /tmp/terraform_${TERRAFORM_VERSION}_${OS}_amd64.zip -d ${TEMP_DIR}/
 	chmod +x ${TEMP_DIR}/terraform
 
 common_jq_binary:
